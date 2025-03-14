cbdf5610cf8ee472e1f3baa39d937ca3db13a0ce
mike8699 <mikebuntu68@gmail.com>
2025-03-14 01:27:16 -0400

    Initial version of .fpk extraction

---
+++ fpk.py
@@ -0,0 +1,65 @@
+import struct
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class FileEntry:
+    filename: str
+    offset: int
+    size: int
+
+
+class FPK:
+    def __init__(self, fpk_file: Path):
+        self.fpk_file: Path = fpk_file
+        self.file_entries: list[FileEntry] = []
+
+        with open(self.fpk_file, "rb") as f:
+            magic: bytes
+            item_count: int
+            magic, item_count = struct.unpack("<10s I", f.read(0xE))
+
+            assert magic == b"\x06\x00\x00\x00FPK_\x00\x00", "Not a valid FPK file!"
+
+            for _ in range(item_count):
+                item_name_len: int = struct.unpack("<I", f.read(4))[0]
+                print(f"Item name length: {hex(item_name_len)}")
+                item_name = f.read(item_name_len).decode()
+                print(f"Item name: {item_name}")
+
+                zeroes_encountered = 0
+                while zeroes_encountered < 4:
+                    if f.read(1) == b"\x00":
+                        zeroes_encountered += 1
+                    else:
+                        zeroes_encountered = 0
+
+                file_size, file_offset = struct.unpack("<II", f.read(8))
+                print(f"File size: {hex(file_size)}")
+                print(f"File offset: {hex(file_offset)}")
+                print()
+
+                self.file_entries.append(FileEntry(item_name, file_offset, file_size))
+
+    def extract(self, dest_dir: Path):
+        dest_dir.mkdir(parents=True, exist_ok=True)
+        for entry in self.file_entries:
+            with open(dest_dir / entry.filename, "wb") as out_file:
+                with open(self.fpk_file, "rb") as f:
+                    f.seek(entry.offset)
+                    data = f.read(entry.size)
+                    out_file.write(data)
+
+
+def main():
+    src_file = Path(sys.argv[1])
+
+    fpk = FPK(src_file)
+
+    fpk.extract(Path("output"))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
